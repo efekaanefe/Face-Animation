b9c35484196fc7e0b7e2c92ee85a6a43db04db41
efekaanefe <efekaanefe1905@gmail.com>
2023-05-11 16:55:15 +0300

    using dlib get some landmark coordinates

--- face_mesh.py
+++ face_mesh.py
@@ -1,13 +1,36 @@
 # import mediapipe
 import cv2
 import matplotlib.pyplot as plt
+import dlib
 
 img = cv2.imread("messi.jpg")
 
-img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR) 
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
 
-plt.imshow(img)
-plt.show()
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+
+# Detect faces in the grayscale image
+faces = detector(gray, 0)
+print(faces)
+# Loop over each face
+for face in faces:
+    # Use the predictor to find facial landmarks
+    landmarks = predictor(gray, face)
+print(landmarks)
+
+# Loop over the landmark points and extract their (x, y) coordinates
+landmark_points = []
+for i in range(68):
+    x = landmarks.part(i).x
+    y = landmarks.part(i).y
+    landmark_points.append((x, y))
+    print(x, y)
+
+# plt.imshow(img)
+# plt.show()
+
 # if cv2.waitKey(1) & 0xFF == ord('q'):
 #     cv2.destroyAllWindows()
 
