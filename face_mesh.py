cea6efcfb38b7f4a439153131309711540e7690d
efekaanefe <efekaanefe1905@gmail.com>
2023-05-11 15:38:40 +0300

    changed to matplotli

--- face_mesh.py
+++ face_mesh.py
@@ -1,12 +1,18 @@
 # import mediapipe
 import cv2
+import matplotlib.pyplot as plt
 
 img = cv2.imread("messi.jpg")
 
 img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR) 
 
-cv2.imshow("webcam", img)
-cv2.waitKey()
-cv2.destroyAllWindows()
+plt.imshow(img)
+plt.show()
+# if cv2.waitKey(1) & 0xFF == ord('q'):
+#     cv2.destroyAllWindows()
 
+# cv2.imshow("webcam", img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+# print("hello")
 
