014ac4eba443072a265aa9c771029e4e3b0772e8
efekaanefe <efekaanefe1905@gmail.com>
2023-05-29 00:42:19 +0300

    opens video succesfully

--- face_mesh_video.py
+++ face_mesh_video.py
@@ -3,14 +3,11 @@ import cv2
 from landmark_processor import LandmarkProcessor
 import time
 
-cap = cv2.VideoCapture('videos\video00.mp4')
-print(cap.isOpened())
+cap = cv2.VideoCapture('videos\\video00.mp4')
 
 while cap.isOpened():
     success, frame = cap.read()
-    
-    print(success)
-    cv2.imshow('Frame',frame)
+    cv2.imshow('Face animation',frame)
     
     # Press Q on keyboard to  exit
     if cv2.waitKey(25) & 0xFF == ord('q'):
